1521b6169bb9a688afc3cb7cfb20a8f4d313118b
sailirawnang <saili.rawnang@wohlig.com>
2018-04-12 15:16:41 +0530

    Socail login is done for backend

--- config/passport/google.js
+++ config/passport/google.js
@@ -1,6 +1,6 @@
 global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
-global["GoogleclientId"] = "529279279497-hdm2ul03erq4kitk7qlqbf41h6pl8f7p.apps.googleusercontent.com";
-global["GoogleclientSecret"] = "nNZiqXW5U2364QI9--sVIR8B";
+global["GoogleclientId"] = "1066475903870-m92qkam2jainsn3n08p2qleq29gge84h.apps.googleusercontent.com";
+global["GoogleclientSecret"] = "tNChfiCHJixtoQ8t9HkgXHiR";
 
 passport.use(new GoogleStrategy({
         clientId: GoogleclientId,
