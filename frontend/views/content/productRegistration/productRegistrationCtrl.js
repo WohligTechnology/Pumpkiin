3828ee2de71ac1ce539d7ac58a425caaa1206911
kishoribihade <kishori.bihade@gmail.com>
2018-05-11 19:20:13 +0530

    header

---
+++ frontend/views/content/productRegistration/productRegistrationCtrl.js
@@ -0,0 +1,7 @@
+myApp.controller('ProductRegistrationCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http) {
+    $scope.template = TemplateService.getHTML("content/productRegistration/productRegistration.html");
+    TemplateService.title = "product Registration"; //This is the Title of the Website
+    // TemplateService.header = " ";
+    $scope.navigation = NavigationService.getNavigation();
+
+});
