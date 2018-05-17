d8e1091bae1fc37717763a8c57378be4e9ae967a
Srujana <srujana.vemula@wohlig.com>
2018-05-17 11:11:55 +0530

    done

--- frontend/js/directives/directive.js
+++ frontend/js/directives/directive.js
@@ -102,5 +102,20 @@ myApp.directive('img', function ($compile, $parse) {
         };
     })
 
-
+    .directive("moveNextOnMaxlength", function () {
+        return {
+            restrict: "A",
+            link: function ($scope, element) {
+                element.on("input", function (e) {
+                   if (element.val().length == element.attr("maxlength")) {
+                       var $nextElement = element.next();
+                       if ($nextElement.length) {
+                           $nextElement[0].focus();
+                        }
+                    }
+                });
+            }
+        }
+    })
+    
 ;
--- frontend/views/content/login/login.js
+++ frontend/views/content/login/login.js
@@ -3,6 +3,47 @@ myApp.controller('LoginCtrl', function ($scope, TemplateService, NavigationServi
     TemplateService.title = "Login"; //This is the Title of the Website
     TemplateService.header = "";
     TemplateService.footer = "";
+    $scope.formName={};
+    $scope.checkChange = function (value) {
+        console.log("inside",value);
+        switch (value) {
+            case 4:
+                if ($scope.formName.digit4 == "") {
+                    var element = $window.document.getElementById('part3');
+                    if (element)
+                        element.focus();
+                }
+                break;
+
+            case 3:
+                if ($scope.formName.digit3 == "") {
+                    var element = $window.document.getElementById('part2');
+                    if (element)
+                        element.focus();
+                }
+                break;
+
+            case 2:
+                if ($scope.formName.digit2 == "") {
+                    var element = $window.document.getElementById('part1');
+                    if (element)
+                        element.focus();
+                }
+                break;
+
+            case 1:
+                if ($scope.formName.digit1 == "") {
+                    console.log("inside 1st condition",case1);
+                    var element = $window.document.getElementById('part1');
+                    console.log("inside element",element);
+                    if (element)
+                        element.focus();
+                }
+                break;
+            default:
+                console.log("invalid choice");
+        }
+    }
     $scope.navigation = NavigationService.getNavigation();
 
 });
