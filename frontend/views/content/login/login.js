c9f99bde3c0371d50083b6d21980612c2fab0619
sailirawnang <saili.rawnang@wohlig.com>
2018-05-17 17:17:43 +0530

    Done with user integration in frontend

--- frontend/views/content/login/login.js
+++ frontend/views/content/login/login.js
@@ -1,49 +1,121 @@
-myApp.controller('LoginCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http) {
+myApp.controller('LoginCtrl', function ($scope, TemplateService, NavigationService, $timeout, $window, toastr, $http) {
     $scope.template = TemplateService.getHTML("content/login/login.html");
     TemplateService.title = "Login"; //This is the Title of the Website
     TemplateService.header = "";
     TemplateService.footer = "";
-    $scope.formName={};
-    $scope.checkChange = function (value) {
-        console.log("inside",value);
-        switch (value) {
-            case 4:
-                if ($scope.formName.digit4 == "") {
-                    var element = $window.document.getElementById('part3');
-                    if (element)
-                        element.focus();
-                }
-                break;
+    $scope.formName = {};
+    $scope.showsignUp = false;
+    $scope.showLogin = true;
+    $scope.shownameEmail = true;
+    $scope.checkUser = function (data) {
+            $scope.formName = {};
+            $scope.formName.mobile = data.mobile;
+            NavigationService.apiCallWithData("WebUser/verifyUser", data, function (res) {
 
-            case 3:
-                if ($scope.formName.digit3 == "") {
-                    var element = $window.document.getElementById('part2');
-                    if (element)
-                        element.focus();
-                }
-                break;
+                if (res.value == false) {
+                    NavigationService.apiCallWithData("WebUser/save", data, function (res1) {
 
-            case 2:
-                if ($scope.formName.digit2 == "") {
-                    var element = $window.document.getElementById('part1');
-                    if (element)
-                        element.focus();
-                }
-                break;
+                        if (res1.value == true) {
+                            $scope.showsignUp = true;
+                            $scope.showLogin = false;
+                            NavigationService.apiCallWithData("WebUser/sendOtp", data, function (res1) {
 
-            case 1:
-                if ($scope.formName.digit1 == "") {
-                    console.log("inside 1st condition",case1);
-                    var element = $window.document.getElementById('part1');
-                    console.log("inside element",element);
-                    if (element)
-                        element.focus();
+                            });
+                        } else {
+                            console.log("mobile already registered")
+                        }
+
+                    });
+                } else {
+                    $scope.showsignUp = true;
+                    $scope.showLogin = false;
+                    $scope.shownameEmail = false;
+                    NavigationService.apiCallWithData("WebUser/sendOtp", data, function (res1) {
+
+                    });
                 }
-                break;
-            default:
-                console.log("invalid choice");
+            });
+        },
+        $scope.saveUser = function (info) {
+            if (info.digit1 >= 0 && info.digit2 >= 0 && info.digit3 >= 0 && info.digit4 >= 0) {
+                $scope.data = {};
+
+                $scope.data.otp = info.digit1.toString() + info.digit2.toString() + info.digit3.toString() + info.digit4.toString();
+                $scope.data.mobile = info.mobile;
+
+                if (info.email || info.name) {
+                    $scope.data.name = info.name;
+                    $scope.data.email = info.email;
+                    NavigationService.apiCallWithData("WebUser/verifyUserWithOtpWhileSignUP", $scope.data, function (data) {
+
+                        if (data.value == true) {
+                            toastr.success('You have been successfully sign in', 'Sign in Success')
+                        } else {
+                            toastr.warning('Enter valid otp');
+                        }
+                    });
+
+                } else {
+                    NavigationService.apiCallWithData("WebUser/verifyUserWithOtpWhileLogin", $scope.data, function (data) {
+
+                        if (data.value == true) {
+                            toastr.success('You have been successfully logged in', 'Login Success')
+                        } else {
+                            toastr.warning('Enter valid otp');
+                        }
+                    });
+                }
+
+            } else {
+                toastr.warning('Enter all fields');
+            }
+        },
+        $scope.sendOtp = function (data) {
+            NavigationService.apiCallWithData("WebUser/sendOtp", data, function (res1) {
+                toastr.success('Otp send succefully')
+                $scope.formName.digit1 = "";
+                $scope.formName.digit2 = "";
+                $scope.formName.digit3 = "";
+                $scope.formName.digit4 = "";
+            });
+        },
+        $scope.checkChange = function (value) {
+            switch (value) {
+                case 4:
+                    if ($scope.formName.digit4 == "") {
+                        var element = $window.document.getElementById('part3');
+                        if (element)
+                            element.focus();
+                    }
+                    break;
+
+                case 3:
+                    if ($scope.formName.digit3 == "") {
+                        var element = $window.document.getElementById('part2');
+                        if (element)
+                            element.focus();
+                    }
+                    break;
+
+                case 2:
+                    if ($scope.formName.digit2 == "") {
+                        var element = $window.document.getElementById('part1');
+                        if (element)
+                            element.focus();
+                    }
+                    break;
+
+                case 1:
+                    if ($scope.formName.digit1 == "") {
+                        var element = $window.document.getElementById('part1');
+                        if (element)
+                            element.focus();
+                    }
+                    break;
+                default:
+                    console.log("invalid choice");
+            }
         }
-    }
     $scope.navigation = NavigationService.getNavigation();
 
 });
