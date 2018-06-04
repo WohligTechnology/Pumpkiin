2830341e8bc7c083121dc5ad099c96ec3b4c1dd1
Ganesh Kothavale <ganesh.wohlig@gmail.com>
2018-06-04 13:36:19 +0530

    done search box

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -4,4 +4,12 @@ myApp.controller('headerCtrl', function ($scope, TemplateService) {
         $(window).scrollTop(0);
     });
     $.fancybox.close(true);
+    $scope.productList = [{
+        name: "Samsung s7 in Mobile",
+        img: "../img/ticketCreation/warranty.png"
+    }, {
+        name: "Samsung s7 in Mobile Black"
+    }, {
+        name: "Samsung s7 edge"
+    }];
 });
