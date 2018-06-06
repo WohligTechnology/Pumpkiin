3af287558de56ffb8ac7964bf13b0e6dbb8c6170
Ganesh Kothavale <ganesh.wohlig@gmail.com>
2018-06-06 12:43:24 +0530

    done

--- frontend/views/content/productListing/productListingCtrl.js
+++ frontend/views/content/productListing/productListingCtrl.js
@@ -3,48 +3,51 @@ myApp.controller('ProductlistingCtrl', function ($scope, TemplateService, Naviga
     TemplateService.title = "Product Listing"; //This is the Title of the Website
     TemplateService.landingheader = "";
     $scope.navigation = NavigationService.getNavigation();
-    $scope.productInfo=[{
-        "largeimage":"img/iphone.jpeg",
-        "smallimage":"img/ticketCreation/mobile.png",
-        "productname":"Apple iPhone X",
-        "relation":"Sisters Phone",
-        "warranty":"Warranty Exp: 8 months"
-    },{
-        "largeimage":"img/iphone.jpeg",
-        "smallimage":"img/ticketCreation/mobile.png",
-        "productname":"Apple iPhone X",
-        "relation":"Sisters Phone",
-        "warranty":"Warranty Exp: 8 months"
-    },{
-        "largeimage":"img/iphone.jpeg",
-        "smallimage":"img/ticketCreation/mobile.png",
-        "productname":"Apple iPhone X",
-        "relation":"Sisters Phone",
-        "warranty":"Warranty Exp: 8 months"
-    },{
-        "largeimage":"img/iphone.jpeg",
-        "smallimage":"img/ticketCreation/mobile.png",
-        "productname":"Apple iPhone X",
-        "relation":"Sisters Phone",
-        "warranty":"Warranty Exp: 8 months"
-    },{
-        "largeimage":"img/iphone.jpeg",
-        "smallimage":"img/ticketCreation/mobile.png",
-        "productname":"Apple iPhone X",
-        "relation":"Sisters Phone",
-        "warranty":"Warranty Exp: 8 months"
-    },{
-        "largeimage":"img/iphone.jpeg",
-        "smallimage":"img/ticketCreation/mobile.png",
-        "productname":"Apple iPhone X",
-        "relation":"Sisters Phone",
-        "warranty":"Warranty Exp: 8 months"
-    },{
-        "largeimage":"img/iphone.jpeg",
-        "smallimage":"img/ticketCreation/mobile.png",
-        "productname":"Apple iPhone X",
-        "relation":"Sisters Phone",
-        "warranty":"Warranty Exp: 8 months"
+    $scope.productInfo = [{
+        "largeimage": "img/iphone.jpeg",
+        "smallimage": "img/ticketCreation/mobile.png",
+        "productname": "Apple iPhone X",
+        "relation": "Sisters Phone",
+        "warranty": "Warranty Exp: 8 months"
+    }, {
+        "largeimage": "img/iphone.jpeg",
+        "smallimage": "img/ticketCreation/mobile.png",
+        "productname": "Apple iPhone X",
+        "relation": "Sisters Phone",
+        "warranty": "Warranty Exp: 8 months"
+    }, {
+        "largeimage": "img/iphone.jpeg",
+        "smallimage": "img/ticketCreation/mobile.png",
+        "productname": "Apple iPhone X",
+        "relation": "Sisters Phone",
+        "warranty": "Warranty Exp: 8 months"
+    }, {
+        "largeimage": "img/iphone.jpeg",
+        "smallimage": "img/ticketCreation/mobile.png",
+        "productname": "Apple iPhone X",
+        "relation": "Sisters Phone",
+        "warranty": "Warranty Exp: 8 months"
+    }, {
+        "largeimage": "img/iphone.jpeg",
+        "smallimage": "img/ticketCreation/mobile.png",
+        "productname": "Apple iPhone X",
+        "relation": "Sisters Phone",
+        "warranty": "Warranty Exp: 8 months"
+    }, {
+        "largeimage": "img/iphone.jpeg",
+        "smallimage": "img/ticketCreation/mobile.png",
+        "productname": "Apple iPhone X",
+        "relation": "Sisters Phone",
+        "warranty": "Warranty Exp: 8 months"
+    }, {
+        "largeimage": "img/iphone.jpeg",
+        "smallimage": "img/ticketCreation/mobile.png",
+        "productname": "Apple iPhone X",
+        "relation": "Sisters Phone",
+        "warranty": "Warranty Exp: 8 months"
     }]
+    $scope.deleteProduct = function (index) {
+        $scope.productInfo.splice(index, 1);
+    }
 
 });
