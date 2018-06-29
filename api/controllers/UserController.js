a007e476bbad5da9ca7ae1433c572deb2f38fca3
aditya <aditya.ghag@wohlig.com>
2018-06-29 13:13:43 +0530

    changes

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -213,5 +213,18 @@ var controller = {
         }
     },
 
+    getAllMembersForSearch: function (req, res) {
+        if (req.body) {
+            User.getAllMembersForSearch(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: {
+                    message: "Invalid Request"
+                }
+            });
+        }
+    },
+
 };
 module.exports = _.assign(module.exports, controller);
