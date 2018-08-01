0b11e8ab531848828cf49dfc78caa19a97727bd3
aditya <aditya.ghag@wohlig.com>
2018-08-01 18:27:13 +0530

    facebook

--- config/passport/facebook.js
+++ config/passport/facebook.js
@@ -1,7 +1,7 @@
 passport.use(new FacebookStrategy({
         clientID: "622844211417911",
         clientSecret: "77dc40c5df0e0df179a831c2eb09aa0d",
-        callbackURL: "http://wohlig.io/api/user/loginFacebook"
+        callbackURL: "https://pumpkiin.wohlig.co.in/api/user/loginFacebook"
     },
     function (accessToken, refreshToken, profile, done) {
         console.log(accessToken, refreshToken, profile);
