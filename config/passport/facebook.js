b641acc4ae6545382540e92a5956f28f8a10f753
aditya <aditya.ghag@wohlig.com>
2018-08-01 18:21:22 +0530

    facebook

--- config/passport/facebook.js
+++ config/passport/facebook.js
@@ -1,18 +1,10 @@
-global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
-global["GoogleclientId"] = "622844211417911";
-global["GoogleclientSecret"] = "77dc40c5df0e0df179a831c2eb09aa0d";
-
-// passport.use(new FacebookStrategy({
-//         clientID: FACEBOOK_APP_ID,
-//         clientSecret: FACEBOOK_APP_SECRET,
-//         callbackURL: "http://www.example.com/auth/facebook/callback"
-//     },
-//     function (accessToken, refreshToken, profile, done) {
-//         User.findOrCreate(..., function (err, user) {
-//             if (err) {
-//                 return done(err);
-//             }
-//             done(null, user);
-//         });
-//     }
-// ));
+passport.use(new FacebookStrategy({
+        clientID: "622844211417911",
+        clientSecret: "77dc40c5df0e0df179a831c2eb09aa0d",
+        callbackURL: "http://wohlig.io/api/user/loginFacebook"
+    },
+    function (accessToken, refreshToken, profile, done) {
+        console.log(accessToken, refreshToken, profile);
+        done(profile);
+    }
+));
