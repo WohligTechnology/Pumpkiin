0dce939fc20c3b2658c29e59748852d5a739773e
Chintan Shah <chintan@wohlig.com>
2018-09-29 21:46:54 +0530

    ok

--- config/passport/facebook.js
+++ config/passport/facebook.js
@@ -3,7 +3,7 @@ passport.use(
       clientID: "162258734683832",
       clientSecret: "df28d801138155091db2be77b596176d",
       callbackURL: "https://pumpkiin.wohlig.co.in/api/user/loginFacebook",
-      profileFields: ["email", "name", "id", "profile_pic"]
+      profileFields: ["email", "name", "id"]
     },
     function (accessToken, refreshToken, profile, cb) {
       profile.googleAccessToken = accessToken;
