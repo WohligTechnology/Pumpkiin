10cd4638ef214b4f383b1b0a2686452488d79ed0
Chintan Shah <chintan@wohlig.com>
2018-10-06 13:29:04 +0530

    ok

--- api/responses/socialLogin.js
+++ api/responses/socialLogin.js
@@ -1,32 +1,36 @@
-module.exports = function (profile) {
-    var req = this.req;
-    var res = this.res;
-    var sails = req._sails;
-    console.log("profileprofileprofile", profile)
-    if (_.isEmpty(profile)) {
-        console.log("prof")
-        res.redirect(env.realHost + "/loogin/");
-        // res.serverError();
-    } else {
-        User.existsSocial(profile, function (err, data) {
-            if (data.name == "noAccess") {
-                data.accessToken[0] = "AccessNotAvailable";
-                res.redirect("http://backend.pumpkiin.com/#!/login" + "/" + data.accessToken[0]);
-                req.session.destroy(function () {});
-            } else {
-                if (err || !data) {
-                    res.callback(err, data);
-                } else {
-                    if (!data.accessLevel) {
-                        data.accessToken[0] = "AccessNotAvailable";
-                    }
-                    console.log(req.session.returnUrl);
-                    console.log("asaasa")
+module.exports = function(profile) {
+  var req = this.req;
+  var res = this.res;
+  var sails = req._sails;
+  console.log("profileprofileprofile", profile);
+  if (_.isEmpty(profile)) {
+    console.log("prof");
+    res.redirect(env.realHost + "/login/");
+    // res.serverError();
+  } else {
+    User.existsSocial(profile, function(err, data) {
+      if (data.name == "noAccess") {
+        data.accessToken[0] = "AccessNotAvailable";
+        res.redirect(
+          "http://backend.pumpkiin.com/#!/login" + "/" + data.accessToken[0]
+        );
+        req.session.destroy(function() {});
+      } else {
+        if (err || !data) {
+          res.callback(err, data);
+        } else {
+          if (!data.accessLevel) {
+            data.accessToken[0] = "AccessNotAvailable";
+          }
+          console.log(req.session.returnUrl);
+          console.log("asaasa");
 
-                    res.redirect("http://backend.pumpkiin.com/#!/login" + "/" + data.accessToken[0]);
-                    req.session.destroy(function () {});
-                }
-            }
-        });
-    }
+          res.redirect(
+            "http://backend.pumpkiin.com/#!/login" + "/" + data.accessToken[0]
+          );
+          req.session.destroy(function() {});
+        }
+      }
+    });
+  }
 };
