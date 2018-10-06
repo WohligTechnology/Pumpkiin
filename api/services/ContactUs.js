69664d88f12dbd5ea9712c741307d41248fe5599
Manish Patil <manish.patil@wohlig.com>
2018-10-06 11:29:14 +0530

    Email of contact us changed

--- api/services/ContactUs.js
+++ api/services/ContactUs.js
@@ -1,49 +1,62 @@
 var schema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        uniqueCaseInsensitive: true,
-        excel: {
-            name: "Name"
-        }
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    uniqueCaseInsensitive: true,
+    excel: {
+      name: "Name"
     }
+  }
 });
 
 schema.plugin(deepPopulate, {});
 schema.plugin(uniqueValidator);
 schema.plugin(timestamps);
-module.exports = mongoose.model('ContactUs', schema);
+module.exports = mongoose.model("ContactUs", schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
 var model = {
+  sendEmail: function(data, callback) {
+    var email = {};
+    email = data.email;
+    Config.sendEmail(
+      {
+        //from
+        email: "sahil@pumpkiin.com",
+        name: "Sahil"
+      },
+      //to
+      //emailData,
+      [
+        {
+          email: "sahil@pumpkiin.com"
+        }
+      ],
 
-    sendEmail: function (data, callback) {
-        var email = {};
-        email = data.email;
-        Config.sendEmail({
-                //from
-                email: 'jagruti@wohlig.com',
-                name: "Jagruti"
-            },
-            //to                    
-            //emailData,
-            [{
-                email: "jagruti@wohlig.com"
-            }],
-
-
-            //subject
-            "User Enquiry!.", "<html><body><h4>User has tried to contact you, Please find the details below:</h4> <p>" + "Name: " + data.name +
-            "</p><p>Email: " + data.email + "</p><p>Phone: " + data.mobile + "</p><p>Subject: " + data.subject +
-            "</p><p>Description: " + data.description + "</p> </body>Thankyou</html>", [],
-            function (err, emailResp) {
-                if (err) {
-                    callback("canNotSendMail", null);
-                } else {
-                    callback(null, "mailSent");
-                }
-            });
-    }
+      //subject
+      "User Enquiry!.",
+      "<html><body><h4>User has tried to contact you, Please find the details below:</h4> <p>" +
+        "Name: " +
+        data.name +
+        "</p><p>Email: " +
+        data.email +
+        "</p><p>Phone: " +
+        data.mobile +
+        "</p><p>Subject: " +
+        data.subject +
+        "</p><p>Description: " +
+        data.description +
+        "</p> </body>Thankyou</html>",
+      [],
+      function(err, emailResp) {
+        if (err) {
+          callback("canNotSendMail", null);
+        } else {
+          callback(null, "mailSent");
+        }
+      }
+    );
+  }
 };
 module.exports = _.assign(module.exports, exports, model);
