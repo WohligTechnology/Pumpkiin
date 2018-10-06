e318223d9bb7de447b1c193e7c111a4d7a615c54
Chintan Shah <chintan@wohlig.com>
2018-10-06 13:28:36 +0530

    ok

--- api/responses/socialLoginGoogle.js
+++ api/responses/socialLoginGoogle.js
@@ -1,35 +1,37 @@
-module.exports = function (profile) {
-    var req = this.req;
-    var res = this.res;
-    var sails = req._sails;
-    console.log("profileprofileprofile", profile)
-    if (_.isEmpty(profile)) {
-        res.redirect(env.realHost + "/login/");
-        // res.serverError();
-    } else {
-        console.log("********************req.session.returnUrl*********************", req.session.returnUrl)
+module.exports = function(profile) {
+  var req = this.req;
+  var res = this.res;
+  var sails = req._sails;
+  console.log("profileprofileprofile", profile);
+  if (_.isEmpty(profile)) {
+    res.redirect(env.realHost + "/login/");
+    // res.serverError();
+  } else {
+    console.log(
+      "********************req.session.returnUrl*********************",
+      req.session.returnUrl
+    );
 
+    User.existsSocialFrontendLogin(profile, function(err, data) {
+      // console.log("*****************************************")
+      // console.log("data22222222222222222222222222222222", data)
 
-        User.existsSocialFrontendLogin(profile, function (err, data) {
-            // console.log("*****************************************")
-            // console.log("data22222222222222222222222222222222", data)
-
-            if (data.name == "noAccess") {
-                data.accessToken[0] = "AccessNotAvailable";
-                res.redirect("https://pumpkiin.com/login/" + "/" + data.accessToken[0]);
-                req.session.destroy(function () {});
-            } else {
-                if (err || !data) {
-                    res.callback(err, data);
-                } else {
-                    if (!data.accessLevel) {
-                        data.accessToken[0] = "AccessNotAvailable";
-                    }
-                    console.log(req.session.returnUrl);
-                    res.redirect("https://pumpkiin.com/login/" + data.accessToken[0]);
-                    req.session.destroy(function () {});
-                }
-            }
-        });
-    }
+      if (data.name == "noAccess") {
+        data.accessToken[0] = "AccessNotAvailable";
+        res.redirect("https://pumpkiin.com/login/" + data.accessToken[0]);
+        req.session.destroy(function() {});
+      } else {
+        if (err || !data) {
+          res.callback(err, data);
+        } else {
+          if (!data.accessLevel) {
+            data.accessToken[0] = "AccessNotAvailable";
+          }
+          console.log(req.session.returnUrl);
+          res.redirect("https://pumpkiin.com/login/" + data.accessToken[0]);
+          req.session.destroy(function() {});
+        }
+      }
+    });
+  }
 };
