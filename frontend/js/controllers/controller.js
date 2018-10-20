573d3b4d062332ec2a5a822590036f482b509a5d
Chintan Shah <chintan@wohlig.com>
2018-10-20 19:21:59 +0530

    changes for the home page issues

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -1,191 +1,240 @@
 var mySwiper;
-myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http, $uibModal, $state) {
-        $scope.template = TemplateService.getHTML("content/home.html");
-        TemplateService.title = "Home"; //This is the Title of the Website
-        TemplateService.header = "";
-        TemplateService.footer = "";
-        TemplateService.cssMain = "without-padding";
-        $scope.navigation = NavigationService.getNavigation();
-        // $('.select-box').selectpicker({
-        //     dropupAuto: false
-        // });
+myApp
+  .controller("HomeCtrl", function(
+    $scope,
+    TemplateService,
+    NavigationService,
+    $timeout,
+    toastr,
+    $http,
+    $uibModal,
+    $state
+  ) {
+    $scope.template = TemplateService.getHTML("content/home.html");
+    TemplateService.title = "Home"; //This is the Title of the Website
+    TemplateService.header = "";
+    TemplateService.footer = "";
+    TemplateService.cssMain = "without-padding";
+    $scope.navigation = NavigationService.getNavigation();
+    // $('.select-box').selectpicker({
+    //     dropupAuto: false
+    // });
 
-        // console.log("hey")
-        console.log('show me produvt1', $scope.product1)
-        $scope.submitForm = function (data) {
-            console.log("This is it");
-            return new Promise(function (callback) {
-                $timeout(function () {
-                    callback();
-                }, 5000);
-            });
-        };
-        // if(product1 && product2){
-        //     $("#clickSubmit").removeClass("disabled");
-        // }
+    // console.log("hey")
+    $scope.toggleClass = function(val) {
+      if (val == 1) {
+        $scope.showClass1 = !$scope.showClass1;
+      } else if (val == 2) {
+        $scope.showClass2 = !$scope.showClass2;
+      }
+    };
+    console.log("show me produvt1", $scope.product1);
+    $scope.submitForm = function(data) {
+      console.log("This is it");
+      return new Promise(function(callback) {
+        $timeout(function() {
+          callback();
+        }, 5000);
+      });
+    };
+    // if(product1 && product2){
+    //     $("#clickSubmit").removeClass("disabled");
+    // }
 
-        $scope.openContact = function (product1, product2) {
-
-            if (product1 != '                  ' && product2 != '                  ') {
-                $scope.userData = {};
-                if (product1 && product2) {
-                    $scope.userData.subject = "My " + product1 + " needs " + product2;
-                }
-                $scope.contactModal = $uibModal.open({
-                    animation: true,
-                    templateUrl: "views/modal/contact.html",
-                    scope: $scope,
-                    backdrop: 'static'
-                });
-            } else {
-                toastr.error("Select all the fields")
-            }
-
-
-
+    $scope.openContact = function(product1, product2) {
+      if (
+        product1 != "                  " &&
+        product2 != "                  "
+      ) {
+        $scope.userData = {};
+        if (product1 && product2) {
+          $scope.userData.subject = "My " + product1 + " needs " + product2;
         }
-
-        $scope.addUser = function (data) {
-            console.log("data", data);
-            $.jStorage.set("UserName", data);
-
-            NavigationService.apiCallWithData("ContactUs/sendEmail", data, function (result) {
-                console.log("result", result);
-                if (result.value) {
-                    toastr.success("We will Contact you soon")
-                    $state.reload();
-                }
-            })
-        }
-
-        $timeout(function () {
-            mySwiper = new Swiper('.swiper-container', {
-                // Optional parameters
-                direction: 'horizontal',
-                loop: false,
-                slidesPerView: 2,
-                slidesPerGroup: 2,
-                breakpoints: {
-                    // when window width is <= 320px
-                    320: {
-                        slidesPerView: 1,
-                        slidesPerGroup: 1,
-                        spaceBetween: 10
-                    },
-                    // when window width is <= 480px
-                    480: {
-                        slidesPerView: 1,
-                        slidesPerGroup: 1,
-                        spaceBetween: 10
-                    },
-                    // when window width is <= 640px
-                    640: {
-                        slidesPerView: 2,
-                        spaceBetween: 20
-                    }
-                },
-                // Navigation arrows
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-            })
-        }, 500);
-
-        // $scope.rate = 7;
-        // $scope.max = 10;
-        // $scope.isReadonly = false;
-
-        // $scope.hoveringOver = function(value) {
-        //   $scope.overStar = value;
-        //   $scope.percent = 100 * (value / $scope.max);
-        // };
-
-        $scope.ratingStates = [{
-                stateOn: 'glyphicon-ok-sign',
-                stateOff: 'glyphicon-ok-circle'
-            },
-            {
-                stateOn: 'glyphicon-star',
-                stateOff: 'glyphicon-star-empty'
-            },
-            {
-                stateOn: 'glyphicon-heart',
-                stateOff: 'glyphicon-ban-circle'
-            },
-            {
-                stateOn: 'glyphicon-heart'
-            },
-            {
-                stateOff: 'glyphicon-off'
-            }
-        ];
-
-        $scope.product = [{
-                name: 'AC'
-            },
-            {
-                name: 'Desktop'
-            },
-            {
-                name: 'Geyser'
-            },
-            {
-                name: 'Laptop'
-            },
-            {
-                name: 'Microwave'
-            }, {
-                name: 'Mobile'
-            }, {
-                name: 'Musical instrument'
-            }, {
-                name: 'Refrigerator'
-            }, {
-                name: 'Tablet'
-            }, {
-                name: 'TV'
-            }, {
-                name: 'Washing Machine'
-            }, {
-                name: 'Water Purifier'
-            }
-        ];
-        $scope.service = [{
-            name: 'Repair'
-        }, {
-            name: 'Service'
-        }]
-
-        $scope.testimonials = [{
-            "name": "Shreyas",
-            "message": "In such busy times, Pumpkiin is a boon. Loved it!",
-            "rate": "5"
-        }, {
-            "name": "Priti",
-            "message": "No more going through IVRs, or coordinating with service personnel. Pumpkiin does it all! Wonderful!",
-            "rate": "5"
-        }, {
-            "name": "Parth",
-            "message": "Fantastic service, peace of mind.",
-            "rate": "5"
-        }, {
-            "name": "Priyanka",
-            "message": "Just one message, and Pumpkiin took care of the rest. So useful!",
-            "rate": "5"
-        }]
-
-    })
-
-    .controller('LinksCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http) {
-        $scope.template = TemplateService.getHTML("content/links.html");
-        TemplateService.title = "Links"; // This is the Title of the Website
-        $scope.navigation = NavigationService.getNavigation();
-    })
-
-    // Example API Controller
-    .controller('DemoAPICtrl', function ($scope, TemplateService, apiService, NavigationService, $timeout) {
-        apiService.getDemo($scope.formData, function (data) {
-            console.log(data);
+        $scope.contactModal = $uibModal.open({
+          animation: true,
+          templateUrl: "views/modal/contact.html",
+          scope: $scope,
+          backdrop: "static"
         });
+      } else {
+        toastr.error("Select all the fields");
+      }
+    };
+
+    $scope.addUser = function(data) {
+      console.log("data", data);
+      $.jStorage.set("UserName", data);
+
+      NavigationService.apiCallWithData("ContactUs/sendEmail", data, function(
+        result
+      ) {
+        console.log("result", result);
+        if (result.value) {
+          toastr.success("We will Contact you soon");
+          $state.reload();
+        }
+      });
+    };
+
+    $timeout(function() {
+      mySwiper = new Swiper(".swiper-container", {
+        // Optional parameters
+        direction: "horizontal",
+        loop: false,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        breakpoints: {
+          // when window width is <= 320px
+          320: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 10
+          },
+          // when window width is <= 480px
+          480: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 10
+          },
+          // when window width is <= 640px
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20
+          }
+        },
+        // Navigation arrows
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      });
+    }, 500);
+
+    // $scope.rate = 7;
+    // $scope.max = 10;
+    // $scope.isReadonly = false;
+
+    // $scope.hoveringOver = function(value) {
+    //   $scope.overStar = value;
+    //   $scope.percent = 100 * (value / $scope.max);
+    // };
+
+    $scope.ratingStates = [
+      {
+        stateOn: "glyphicon-ok-sign",
+        stateOff: "glyphicon-ok-circle"
+      },
+      {
+        stateOn: "glyphicon-star",
+        stateOff: "glyphicon-star-empty"
+      },
+      {
+        stateOn: "glyphicon-heart",
+        stateOff: "glyphicon-ban-circle"
+      },
+      {
+        stateOn: "glyphicon-heart"
+      },
+      {
+        stateOff: "glyphicon-off"
+      }
+    ];
+
+    $scope.product = [
+      {
+        name: "AC"
+      },
+      {
+        name: "Desktop"
+      },
+      {
+        name: "Geyser"
+      },
+      {
+        name: "Laptop"
+      },
+      {
+        name: "Microwave"
+      },
+      {
+        name: "Mobile"
+      },
+      {
+        name: "Musical instrument"
+      },
+      {
+        name: "Refrigerator"
+      },
+      {
+        name: "Tablet"
+      },
+      {
+        name: "TV"
+      },
+      {
+        name: "Washing Machine"
+      },
+      {
+        name: "Water Purifier"
+      }
+    ];
+    $scope.service = [
+      {
+        name: "Repair"
+      },
+      {
+        name: "Service"
+      }
+    ];
+
+    $scope.testimonials = [
+      {
+        name: "Shreyas",
+        message: "In such busy times, Pumpkiin is a boon. Loved it!",
+        rate: "5"
+      },
+      {
+        name: "Priti",
+        message:
+          "No more going through IVRs, or coordinating with service personnel. Pumpkiin does it all! Wonderful!",
+        rate: "5"
+      },
+      {
+        name: "Parth",
+        message: "Fantastic service, peace of mind.",
+        rate: "5"
+      },
+      {
+        name: "Priyanka",
+        message:
+          "Just one message, and Pumpkiin took care of the rest. So useful!",
+        rate: "5"
+      }
+    ];
+  })
+
+  .controller("LinksCtrl", function(
+    $scope,
+    TemplateService,
+    NavigationService,
+    $timeout,
+    toastr,
+    $http
+  ) {
+    $scope.template = TemplateService.getHTML("content/links.html");
+    TemplateService.title = "Links"; // This is the Title of the Website
+    $scope.navigation = NavigationService.getNavigation();
+  })
+
+  // Example API Controller
+  .controller("DemoAPICtrl", function(
+    $scope,
+    TemplateService,
+    apiService,
+    NavigationService,
+    $timeout
+  ) {
+    apiService.getDemo($scope.formData, function(data) {
+      console.log(data);
     });
+  });
