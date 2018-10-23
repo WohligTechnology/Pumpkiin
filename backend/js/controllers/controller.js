4f3fbaf5258c0afc97731692250a314110ceb5f5
Manish Patil <manish.patil@wohlig.com>
2018-10-23 20:07:22 +0530

    Changes

--- backend/js/controllers/controller.js
+++ backend/js/controllers/controller.js
@@ -2199,7 +2199,7 @@ myApp
         if (data == "Awaiting feedback") {
           $scope.ticketData.feedbackStatus = true;
           formData.user = $scope.jstrgData._id;
-          formData.comment = "Dear " + $scope.ticketData.user.name + ", Thank you for providing us with the opportunity to serve you. We hope we were able to do so with satisfaction. Request if you could kindly provide us with your valuable feedback below. This will greatly help us improve our service. Thank you, Team Pumpkiin";
+          formData.comment = "Thank you for providing us with the opportunity to serve you. We hope we were able to do so with satisfaction. Request if you could kindly provide us with your valuable feedback below. This will greatly help us improve our service. Thank you, Team Pumpkiin";
 
           $scope.ticketData.customerChat.push(formData);
 
